6f4177e6146116055903039d0e55c7760b86a5ae
snjmrym <usareru@gmail.com>
2016-07-16 18:45:48 +0900

    first commit

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+	public GameObject clearUI;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player"){
+			//print ("hello!");
+			clearUI.SetActive (true);
+		}
+	}
+}
---
+++ Assets/Scripts/UIButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButtonScript : MonoBehaviour {
+
+	public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick(){
+		Application.LoadLevel (nextScene);
+	}
+}
